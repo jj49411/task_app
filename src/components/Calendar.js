84a31fca36c9ffac217d3c8d7727ca64b652f6ee
Julie <j49411@gmail.com>
2020-08-18 17:16:58 +0100

    feat: created dateClick function

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from "@fullcalendar/interaction"; 
 
 export default class Calendar extends React.Component {
+
+  handleDateClick = (arg) => { 
+    alert(arg.dateStr)
+  }
+
   render() {
     return (
       <FullCalendar
-        plugins={[ dayGridPlugin ]}
+        plugins={[ dayGridPlugin, interactionPlugin ]}
         initialView="dayGridMonth"
+        dateClick={this.handleDateClick}
       />
     )
   }
